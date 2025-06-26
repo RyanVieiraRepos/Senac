a7d9f10b9d9dafc4c7c30240008b3b0751424b4f
ryanviera05 <ryan.assumpcao@outlook.com>
2025-06-26 19:27:21 -0300

    commit

--- Aulas/Javascript/Desafios/dia12/app.js
+++ Aulas/Javascript/Desafios/dia12/app.js
@@ -1,31 +1,16 @@
+var nome, nota1, nota2, nota3, nota4, media;
 
-/*var contador = 10;
-while (contador >= 0 ){
-    console.log("-----" + contador +"----")
-    contador = contador - 1
+var resposta = true;
+
+while (resposta == true) {
+    nome = prompt("Informe o nome do aluno(a)", "Digite aqui");
+    nota1 = prompt("Informe a nota do 1º bimestre", "Digite aqui");
+    nota2 = prompt("Informe a nota do 2º bimestre", "Digite aqui");
+    nota3 = prompt("Informe a nota do 3º bimestre", "Digite aqui");
+    nota4 = prompt("Informe a nota do 4º bimestre", "Digite aqui");
+    media = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3)
+        + parseFloat(nota4)) / 4;
+    alert("O(A) estudande " + nome + " obteve a media " + media);
+    resposta = confirm("Calcular a media de outro aluno?");
 }
 
-console.log("BOOM");
-*/
-
-/*let contador =1;
-let numeroRepeticao = prompt("Informe o numero de vezes que a mensagem deve aparecer");
-
-while(contador <= numeroRepeticao){
-    console.log("Mensagem numero " + contador);
-    contador = contador + 1;
-}
-
-*/
-
-
-
-let base = prompt("A contagem iniciará em qual número ?");
-
-while(base >= 0){
-    console.log("----" + base + "----");
-    base = base - 1;
-}
-
-
-console.log("BOOM")
