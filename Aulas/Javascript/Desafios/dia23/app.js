88537882f635b02d276542e2be0809a6a6b0f33b
Ryan Vieira Assumpção <ryanvieiramail@gmail.com>
2025-08-28 20:34:41 -0300

    Desafios javascript dia 23

---
+++ Aulas/Javascript/Desafios/dia23/app.js
@@ -0,0 +1,23 @@
+var media = 0, nota = 0, contador = 0
+var nome = prompt("Informe o nome do aluno", "Digite Aqui!!!") 
+
+
+
+
+
+
+while (contador > 4){ //enquanto o valor de contador for menor que 4, o usuário através de um prompt, preencherá a variável nota, se o valor de nota não for um número, será exibido uma mensagem no navegador, caso se ja informado um número, a variável media conterá a soma da media e da nota, será somao 1 ao contador, fazendo com que, depois de 4 ciclos, o navegador pare de abrir novos prompts  
+nota = prompt("Informe a nota do aluno","Digite aqui")
+if(isNaN(nota))
+  alert("Voce não digitou um numero valido")
+else{
+  media = parseFloat(media) + parseFloat(nota);
+  contador++;
+   }
+}
+//IsNaN() está verificando se a var nota é um número 
+
+media - media/4;
+
+document.write("A média de " + nome) // dois valores e dois textos serão inseridos no html
+document.write(" é " + media.toFixed(1)); //a função toFixed() corta a casa decimal de acordo com o valor parâmetrado
