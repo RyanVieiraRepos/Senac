b45f73325a88f82cbc88f875d2e30224b2f548e7
TatianeAlves13 <alves1395.thatyanne@gmail.com>
2025-10-01 18:53:16 -0300

    makrama js - header,footer and home css

---
+++ Projetos/Makrama Sep/Components/Header/Header.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+`
+<div class="homeCont">
+        <div class="homeText">
+          <strong>Artesanais que encantam!</strong>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et
+            cursus metus. Quisque sed rhoncus nisi. Mauris quis tincidunt augue.
+            Proin in ante turpis. Aenean interdum tellus sed nibh fringilla
+            ullamcorper. Aenean nec quam non felis iaculis euismod mollis non
+            augue. Etiam facilisis pretium nulla tincidunt finibus. Praesent
+            mattis dignissim lacinia. Cur.
+          </p>
+
+          <button type="button" id="homebutton">Navegar!</button>
+        </div>
+        <div class="homeImage">
+          <img src="./Components//Pages/Home/images/enfeite1 ai.png" />
+        </div>
+      </div>
+`
