bf1ee63dff6f895c14383bcc158c1f37ccb3d829
Ryan Vieira Assumpção <ryanvieiramail@gmail.com>
2025-10-06 22:25:51 -0300

    Javascript - exercicio 14 - adicionando vaga e veiculo à uma tabela

---
+++ Aulas/Javascript/Exercícios - Setembro.25/Exercício014/table.js
@@ -0,0 +1,41 @@
+class Produtos extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        
+         <div class="produtosCont">
+
+      <div class="produtosHead">
+        <strong>Produtos</strong>
+        <div class="produtosSearchCont">
+          <input placeholder="Pesquise">
+          <button>Adicionar Produto</button>
+          <select>
+
+            <option>Ordem Alfabética</option>
+            <option>Preço</option>
+            <option>Quantidade de Pedidos</option>
+          </select>
+
+
+
+        </div>
+
+      </div>
+
+      <div class="tableHead">
+
+        <strong>Vaga</strong>
+        
+        <strong>Veículo</strong>
+
+        <strong>Ações</strong>
+      </div>
+
+
+      <div class="table" id="table">
+      </div>
+
+    </div>
+        `
+    }
+} customElements.define("dash-produtos", Produtos)
